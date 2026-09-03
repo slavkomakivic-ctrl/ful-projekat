12c6fd0a18f30f439fdcd3ed180931620a7dcdd7
Slavko Makivic <slavkomakivic@gmail.com>
2026-09-03 20:17:43 +0200

    Dodavanje bazi korisnici.db i baza_knjige_autori.db

--- database.py
+++ database.py
@@ -1,13 +1,35 @@
 import sqlite3
 
-konekcija = sqlite3.connect("korisnici.db", check_same_thread=False)
-kursor = konekcija.cursor()
+konekcija1 = sqlite3.connect("korisnici.db", check_same_thread=False)
+kursor1 = konekcija1.cursor()
 
-kursor.execute("""
+kursor1.execute("""
     CREATE TABLE IF NOT EXISTS korisnici (
         id INTEGER PRIMARY KEY,
         korisnicko_ime TEXT UNIQUE,
         lozinka_hash TEXT
+    )
+""")
+konekcija1.commit()
+
+konekcija = sqlite3.connect("baza_knjige_autori.db", check_same_thread=False)
+kursor = konekcija.cursor()
+
+kursor.execute("""
+    CREATE TABLE IF NOT EXISTS autori(
+        id INTEGER PRIMARY KEY,
+        ime TEXT
+    )
+""")
+
+kursor.execute("""
+    CREATE TABLE IF NOT EXISTS knjige (
+        id INTEGER PRIMARY KEY,
+        naslov TEXT,
+        godina INTEGER,
+        ocjena INTEGER,
+        autor_id INTEGER,
+        FOREIGN KEY (autor_id) REFERENCES autori(id)        
     )
 """)
 konekcija.commit()
--- models.py
+++ models.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel, Field
 class Korisnik(BaseModel):
     korisnicko_ime: str = Field(min_length=1, max_length=30)
     lozinka: str = Field(min_length=1, max_length=50)
+
+class Autori(BaseModel):
+    ime: str = Field(min_length=1, max_length=100)
+
+class Knjige(BaseModel):
+    naslov: str = Field(min_length=1, max_length=200)
+    godina: int = Field(gt=1, le=2026)
+    ocjena: int = Field(ge=1, le=10)
